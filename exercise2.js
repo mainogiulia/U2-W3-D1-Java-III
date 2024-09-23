c000f67d27f641d83e047bea42f8af31771ba791
mainogiulia <giuliamaino97@gmail.com>
2024-09-23 16:55:04 +0200

    exercise done

--- exercise2.js
+++ exercise2.js
@@ -1,18 +1,18 @@
-const petNameField = document.getElementsByClassName("petNameField");
-const ownerNameField = document.getElementsByClassName("ownerNameField");
-const petSpeciesField = document.getElementsByClassName("petSpeciesField");
-const petBreedField = document.getElementsByClassName("petBreedField");
-const button = document.getElementsByTagName("button");
-const petList = document.getElementsByClassName("petList");
+const petNameField = document.getElementById("petNameField");
+const ownerNameField = document.getElementById("ownerNameField");
+const petSpeciesField = document.getElementById("petSpeciesField");
+const petBreedField = document.getElementById("petBreedField");
+const button = document.getElementById("button");
+const petList = document.getElementById("petList");
 
 const pets = [];
 
 class Pet {
   constructor(_petName, _ownerName, _species, _breed) {
-    this.petName;
-    this.ownerName;
-    this.species;
-    this.breed;
+    this.petName = _petName;
+    this.ownerName = _ownerName;
+    this.species = _species;
+    this.breed = _breed;
   }
 
   hasSmeOwner(otherPet) {
@@ -25,8 +25,16 @@ class Pet {
 }
 
 const namesList = function () {
+  petList.innerHTML = "";
   pets.forEach((pet) => {
     const listItem = document.createElement("li");
-    listItem.innerText = `Pet's name: ${}`
+    listItem.innerText = `Pet's name: ${pet.petName}; Owner's name: ${pet.ownerName}; Pet's species: ${pet.species}; Pet's breed: ${pet.breed}.`;
+    petList.appendChild(listItem);
   });
+};
+
+button.onclick = function () {
+  let petInfo = new Pet(petNameField.value, ownerNameField.value, petSpeciesField.value, petBreedField.value);
+  pets.push(petInfo);
+  namesList();
 };
